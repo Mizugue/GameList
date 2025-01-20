8ce60305c56690ac73acc18ad4189b8cc0423ad4
Mzg. <jchallaksantos@gmail.com>
2025-01-20 15:26:12 -0300

    start

---
+++ src/main/java/com/mzg/bicholist/BicholistApplication.java
@@ -0,0 +1,13 @@
+package com.mzg.bicholist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicholistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicholistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mzg/bicholist/BicholistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzg.bicholist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicholistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
